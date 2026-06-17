a10ca87b975f5afdf5abc8c91098ba1b7203a783
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-06-17 08:12:50 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API is Running...");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    message: "Backend Working Successfully"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
+//Password DB: SSnh4YgvOAVTuq8y
